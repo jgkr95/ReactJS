4acca56ad9ba2c85977f6e9e3282bbd9784108a0
jgkr95 <jgkr95@msitprogram.net>
2020-12-09 19:59:47 +0530

    Google notes

---
+++ Google Keep/google-keep/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+         <div className="header">
+             <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="logo" height="70" width ="70"/>
+            <h4> Keep </h4>
+         </div>
+    )
+}
+
+export default Header
